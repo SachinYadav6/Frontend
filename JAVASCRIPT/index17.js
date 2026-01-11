8ae6b85da38011b00af3ccd86b2b2fa940be806f
Sachin Yadav <ksachin1811@gmail.com>
2026-01-12 04:46:05 +0530

    add method of string and array

---
+++ JAVASCRIPT/index17.js
@@ -0,0 +1,17 @@
+// string 
+let s = 's';
+console.log(s);
+
+let fullName = ' TONY STARK     ';
+console.log(fullName.indexOf('T'));
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());
+console.log(fullName.trim());
+console.log(fullName.slice(2, 4));
+console.log(fullName.repeat(3));
+console.log(fullName.replace("T","S"));
+
+
+
+
+
---
+++ JAVASCRIPT/index18.js
@@ -0,0 +1,64 @@
+// if else
+let name = 'sachin';
+
+if (name.length > 4 && name === "sachin") {
+    console.log('sachin kumar from bhopal');
+    
+} else {
+    console.log('not print');
+    
+}
+// traffic
+
+let color = 're';
+if (color == "red") {
+    console.log("break the car");
+    
+    
+} else if (color == "yellow") {
+    console.log("slow the car");
+    
+    
+} else if (color == "green") {
+    console.log("go");
+    
+} else {
+    console.log("light is broken.");
+    
+}
+
+// switch
+
+let days = 1;
+
+switch (days) {
+    case 1:
+        console.log("monday");
+        
+        
+        break;
+    
+    case 2:
+        console.log("tuesday");
+        break;
+    
+    case 3:
+        console.log("wednesday");
+        break;
+    
+    case 4:
+        console.log("thrusday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+        console.log("saturday");
+        break;
+    case 7:
+        console.log(sunday);
+        break;
+        
+    default:
+        break;
+}
