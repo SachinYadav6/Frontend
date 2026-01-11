be7c84ddd54ec7e6e70ef39ca639b940d9910488
Sachin Yadav <ksachin1811@gmail.com>
2026-01-11 17:56:06 +0530

    Add JS examples - variables, assignment operators, string forward/reverse, loops, NaN

---
+++ JAVASCRIPT/index15.js
@@ -0,0 +1,51 @@
+// operator in js 
+console.log(12+89);
+console.log(89 - 67);
+console.log(78*6);
+console.log(12/6);
+console.log(12%5);
+console.log(12**2);
+
+console.log(0 / 0);
+console.log(NaN - 1);
+console.log(NaN+1);
+
+// syntex declare variable ok assinment operator
+let age = 23;
+age += 2;
+console.log(age);
+
+let sum = 0;
+console.log(sum);
+
+sum = sum + 2;
+console.log(sum);
+sum += 4;
+console.log(sum);
+
+x = 'sachin';
+y = 'kumar';
+x += y;
+console.log(x);
+
+let str='sachin'
+ rachin=str[0] = 'R' + str.slice(1);
+console.log(rachin);
+
+let r = 'rahul';
+for (let i = 0; i < r.length - 1; i++){
+    console.log(r[i]);
+    
+}
+console.log('reverse the string');
+
+let s = 'ABC';
+let result = '';
+for (let i = s.length-1; i >= 0; i--){
+    result = result + s[i];
+    console.log(result);
+    
+}
+
+
+
