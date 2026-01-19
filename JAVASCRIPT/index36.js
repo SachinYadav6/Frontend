65fa5877af88422a70885c369b7939c4539c0bb7
Sachin Yadav <ksachin1811@gmail.com>
2026-01-19 20:22:28 +0530

    add datatype of js revision

---
+++ JAVASCRIPT/index36.js
@@ -0,0 +1,31 @@
+/* java datatypes revision*/ 
+let name = 'sachin kumar'
+    ;
+    console.log(typeof name);
+
+
+let roll;
+console.log(typeof roll);
+
+
+let x=null;
+console.log(typeof x);
+
+let d = BigInt("567");
+console.log(typeof d);
+
+let e = BigInt("456") + BigInt("567");
+console.log(typeof e);
+
+
+let n = 6778;
+console.log(typeof n);
+
+let result = false;
+console.log(typeof result);
+
+
+
+
+
+    
