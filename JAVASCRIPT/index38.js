767e485d7e49d4ef0f9c129b329c987aa094fac7
Sachin Yadav <ksachin1811@gmail.com>
2026-01-20 21:21:58 +0530

    add revision object array method

---
+++ JAVASCRIPT/index38.js
@@ -0,0 +1,37 @@
+/* array*/ 
+let student_colllection_information = [{
+    id: 101,
+    name: 'sachin kumar',
+    father_name:'basudev yadav'
+
+},
+    {
+        id: 102,
+        name: ' komal kumarai',
+        father_name: 'satendra yadav'
+
+    },
+    {
+        id: 103,
+        name: 'sachin kumar',
+        father_name: 'satendra kumar yadav'
+
+    }, {
+        id: 104,
+        name: 'suney  kumar',
+        father_name: 'sudev yadav'
+
+    }, {
+        id: 105,
+        name: 'niraj kumar',
+        father_name: 'kelash yadav'
+
+    }, {
+        id: 106,
+        name: 'sonu kumar',
+        father_name: 'raju yadav'
+
+    }]
+
+    console.log(student_colllection_information);
+    
