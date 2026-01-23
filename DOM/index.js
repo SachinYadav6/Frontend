b0ead558345e8a319b88ca5d233140db6080bfaf
Sachin Yadav <ksachin1811@gmail.com>
2026-01-23 15:56:35 +0530

    add the concepts readout getElementById/ClassName and Uninary selector

---
+++ DOM/index.js
@@ -0,0 +1,17 @@
+let p1 = document.getElementById("kumar");
+console.log(p1);
+
+let p2 = document.getElementsByClassName("sachin");
+console.log(p2);
+
+ let tagnames=document.getElementsByTagName('p');
+console.log(tagnames);
+
+
+let para = document.querySelector('.sachin');
+console.log(para);
+ let para2 = document.querySelector('#kumar');
+console.log(para2);
+
+
+
