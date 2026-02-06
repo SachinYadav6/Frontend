5d93bc25647c6c6bb4f26daf36706168ca7803de
Sachin Yadav <ksachin1811@gmail.com>
2026-02-06 23:47:00 +0530

    Fix favouriteMovie comparison in while loop

---
+++ JAVASCRIPT/index64.js
@@ -0,0 +1,13 @@
+let favouriteMovie = 'spiderMan';
+while (true) {
+    if (favouriteMovie === 'spiderMan') {
+        console.log('movie was correct');
+        
+        break;
+    } else {
+        console.log('try again');
+        favouriteMovie='spiderMan'//value chang
+        
+    }
+}
+
