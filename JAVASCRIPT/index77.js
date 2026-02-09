2a91899b0b0d5088e7c495b63f21f04909943f63
Sachin Yadav <ksachin1811@gmail.com>
2026-02-09 20:48:55 +0530

    Practice code for all JavaScript loops

---
+++ JAVASCRIPT/index77.js
@@ -0,0 +1,18 @@
+// while loop example
+let i = 1;
+while (i <= 5) {
+    console.log("While loop count:", i);
+    i++;
+}
+
+// for loop example
+for (let i = 1; i <= 5; i++) {
+    console.log("For loop count:", i);
+}
+
+// do while loop example
+let j = 1;
+do {
+    console.log("Do while loop count:", j);
+    j++;
+} while (j <= 5);
