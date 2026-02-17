389df86ad28671e5aa406f93faebf822ff023e8a
Sachin Yadav <ksachin1811@gmail.com>
2026-02-17 16:09:59 +0530

    update the destructuring in object

---
+++ JAVASCRIPT/index86.js
@@ -0,0 +1,13 @@
+let n = {
+    n: 'sachin kumar',
+    r:'0179cs221068'
+}
+console.log(n);
+/* destructering the object*/
+console.log('destructure');
+
+let {n: firstname,r: roll } = n;
+console.log(firstname);
+console.log(roll);
+
+
