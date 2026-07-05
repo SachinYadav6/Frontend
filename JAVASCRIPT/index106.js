10cc7a341e12c8ff076510e19abb9bd3594b261e
Sachin Yadav <ksachin1811@gmail.com>
2026-07-05 23:58:09 +0530

    revise object

---
+++ JAVASCRIPT/index106.js
@@ -0,0 +1,11 @@
+let object = {
+    name: 'sachin kumar',
+    fathername: 'Basudev yadav',
+    mothername: 'Mohani devi',
+    
+}
+console.log(object);
+console.log(object.fathername);
+console.log(object.mothername);
+
+
