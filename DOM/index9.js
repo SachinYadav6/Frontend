f44f06937795ea70a12e0672d084d168a5cb060d
Sachin Yadav <ksachin1811@gmail.com>
2026-07-11 19:59:21 +0530

    Practice DOM traversal methods and element relationships

---
+++ DOM/index9.js
@@ -0,0 +1,16 @@
+let div = document.querySelector('#parent');
+console.log(div.childElementCount, div.parentElement, div.childElementCount);
+console.log(div.firstElementChild);
+console.log(div.lastElementChild);
+console.log(div.nextElementSibling);
+console.log(div.previousElementSibling);
+let h1 = document.querySelector('h1');
+console.log(h1.parentElement);
+let p = document.querySelector('p');
+console.log(p);
+console.log(p.previousElementSibling);
+console.log(p.nextElementSibling);
+
+
+
+
