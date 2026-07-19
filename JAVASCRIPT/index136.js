a1c080421dfb40ab8a35a1675070994ad781263b
Sachin Yadav <ksachin1811@gmail.com>
2026-07-19 23:28:56 +0530

    Practice JavaScript object creation and dot notation

---
+++ JAVASCRIPT/index136.js
@@ -0,0 +1,19 @@
+let student = {
+    name: "Sachin",
+    age: 22,
+    course: "BTech"
+};
+
+console.log(student);
+
+/*
+ *dot iteration*/
+let student1 = {
+    name: "Sachin",
+    age: 22,
+    course: "BTech"
+};
+
+console.log(student1.name);
+console.log(student1.age);
+console.log(student1.course);
