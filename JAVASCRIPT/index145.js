709bf967b5de3729ae91cb395288a62c31f64c3a
Sachin Yadav <ksachin1811@gmail.com>
2026-07-25 23:55:37 +0530

    Learn forEach callback function

---
+++ JAVASCRIPT/index145.js
@@ -0,0 +1,18 @@
+/* 
+array.forEach((currentValue, index, array) => {
+  
+});
+foreach()
+*/
+
+let number = [10, 20, 30];
+const num = number.forEach((currentValue, index, array) => {
+    console.log(currentValue, index, array);
+    
+})
+     
+console.log(num);
+
+   
+    
+
