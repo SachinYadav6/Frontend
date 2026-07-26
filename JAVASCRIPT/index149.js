af0f741fe61b8f93084cffeef58903a73014254f
Sachin Yadav <ksachin1811@gmail.com>
2026-07-26 23:16:00 +0530

    Add async/await fetch API example

---
+++ JAVASCRIPT/index149.js
@@ -0,0 +1,13 @@
+async function getUsers() {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+        const users = await response.json();
+
+        console.log(users);
+    } catch (error) {
+        console.log("Error:", error);
+    }
+}
+
+getUsers();
