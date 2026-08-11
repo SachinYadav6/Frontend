e0b3e0395b2d2f0eec3b69ceee28eda2303c7d45
Sachin Yadav <ksachin1811@gmail.com>
2026-08-11 13:28:05 +0530

    feat: add Java primitive data types examples

---
+++ JAVA/01-basics/type.java
@@ -0,0 +1,43 @@
+public class type {
+    public static void main(String[] args) {
+        /* primitive data types */ 
+
+        /* integer*/
+        int a = 45;
+        int b = 34;
+        int sum = a + b;
+        System.out.println(sum);
+        /* boolean*/  
+        boolean pass = true;
+        System.out.println(pass);
+        /* float*/ 
+        float number = 34.5678f;
+        System.out.println(number);
+
+        /* char*/
+        char alphabet = 'a';
+        System.out.println(alphabet);
+        /* string*/ 
+        String name = "sachin kumar";
+        System.out.println(name);
+        /* long*/
+        long largenumber = 12345566677L;
+        System.out.println(largenumber);
+
+        /* byte*/
+
+        byte bytes = 123;
+        System.out.println(bytes);
+
+        /* double*/
+        
+        double decimalNumber = 12455.677888;
+        System.out.println(decimalNumber);
+
+        /* short*/ 
+        short num = 234;
+        System.out.println(num);
+    }
+
+    
+}
