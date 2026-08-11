936f7920e5f73a3b3f642ae5b42b52734e460fcd
Sachin Yadav <ksachin1811@gmail.com>
2026-08-12 03:56:21 +0530

    feat: add basic arithmetic operators example

---
+++ JAVA/01-basics/operator.java
@@ -0,0 +1,15 @@
+public class operator {
+    public static void main(String[] args) {
+       double a = 5;
+       double b = 90;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    
+    }
+
+
+    
+}
