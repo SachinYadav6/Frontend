02a8d2a234cfe2fc95a550bd442a380631b7785c
Sachin Yadav <ksachin1811@gmail.com>
2026-08-13 18:38:27 +0530

    Add Java data types practice

--- JAVA/01-basics/DataTypes.java
+++ JAVA/01-basics/DataTypes.java
@@ -1,7 +1,31 @@
 public class DataTypes {
     public static void main(String[] args) {
 
-        System.out.println("sachin kumar");
+        String name = "sachin kumar";
+        System.out.println(name);
+
+        byte n = 12;
+        System.out.println(n);
+
+        short num = 90;
+        System.out.println(num);
+
+        float deci = 45.90f;
+        System.out.println(deci);
+
+        double decimalNumber = 234.67789;
+        System.out.println(decimalNumber);
+
+        char alphabet = 'd';
+        System.out.println(alphabet);
+
+        int numbers = 4567;
+        System.out.println(numbers);
+
+        boolean pass = true;
+        System.out.println(pass);
+        long largenumber = 22344556667788L;
+        System.out.println(largenumber);
 
     }
 }
