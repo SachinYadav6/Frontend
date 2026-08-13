facdcd66fcee788fc32dd82e5ad47af521a60abd
Sachin Yadav <ksachin1811@gmail.com>
2026-08-13 19:27:27 +0530

    Practice Java if else conditions

--- JAVA/02-conditions/ifElse.java
+++ JAVA/02-conditions/ifElse.java
@@ -1,7 +1,18 @@
 
 public class ifElse {
     public static void main(String[] args) {
-        System.out.println("ifElse condtion ");
+        int num = 28;
+        if (num >=18) {
+            System.out.println("you are eligible for voting");
+
+        } else if (num >=10 && num <18) {
+            System.out.println("you are teenager");
+
+        } else {
+            System.out.println("yuo are not eigible for vote");
+
+        }
+       
     }
     
 }
