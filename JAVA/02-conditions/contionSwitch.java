dac7d94790a7537c81a6b69bbfeca27d260c86a1
Sachin Yadav <ksachin1811@gmail.com>
2026-08-13 20:14:52 +0530

    Practice Java switch statement

---
+++ JAVA/02-conditions/contionSwitch.java
@@ -0,0 +1,22 @@
+public class contionSwitch {
+public static void main(String[] args) {
+    String fruit = "orange";
+   switch (fruit) {
+        case "orange":
+         System.out.println("orange");
+        
+         break;
+         case "pomigrant":
+            System.out.println("pomigrant");
+            break;
+            case "papaya":
+                System.out.println("papaya");
+                break;
+   
+            default:
+         System.out.println("invalid fruits name");
+         break;
+   }
+}
+    
+}
