a8cbd16cf846a41f280a58cc490b4b3bc1efa67a
Sachin Yadav <ksachin1811@gmail.com>
2026-08-17 00:24:24 +0530

    revise function

---
+++ JAVA/03-function/hell.java
@@ -0,0 +1,15 @@
+public class hell {
+    static void printHello() {
+        System.out.println("hello function");
+            
+
+        }
+
+        public static void main(String[] args) {
+        printHello();
+        
+    }
+
+
+    
+}
