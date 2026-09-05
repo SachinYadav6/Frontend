8bb53aa248f98d5c606c207cb56c194ea54ae8e1
Sachin Yadav <ksachin1811@gmail.com>
2026-09-06 00:04:14 +0530

    implementation of given q is find total count of vowel

---
+++ JAVASCRIPT/index169.js
@@ -0,0 +1,17 @@
+let name = "sachin kumar";
+let countvoel = 0;
+for (let i = 0; i < name.length; i++){
+  
+    if (name [i]=== 'a' ||
+        name[i] === 'i' ||
+        name [i]=== 'e' ||
+        name [i]=== 'u' ||
+        name[i]==='o'
+    ) {
+        countvoel += 1;
+        
+    }
+  
+    
+}
+console.log(countvoel);
