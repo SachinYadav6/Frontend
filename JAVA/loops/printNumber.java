6a84b0ce26e8d7ec252cf55b8b9a1c7956c97442
Sachin Yadav <ksachin1811@gmail.com>
2026-09-09 20:26:56 +0530

    print the number 1 to 10 just for loop revise ok

---
+++ JAVA/loops/printNumber.java
@@ -0,0 +1,9 @@
+public class printNumber {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+    
+}
