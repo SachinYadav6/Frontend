d4f203be694d6000bd638f1624ba63a308315965
Sachin Yadav <ksachin1811@gmail.com>
2026-09-10 20:22:01 +0530

    add linear search for given number of max number

---
+++ JAVA/05-array/findMAXNUMBERGIVENARRAY.java
@@ -0,0 +1,12 @@
+public class findMAXNUMBERGIVENARRAY {
+    public static void main(String[] args) {
+        int[] arr = { 10, 25, 7, 30, 15 };
+        int max = arr[0];
+        for (int i = 0; i <arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+       System.out.println(max);
+    }
+}
