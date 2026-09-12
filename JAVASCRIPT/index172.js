add4cc2e990a704457b150180f48814672323b23
Sachin Yadav <ksachin1811@gmail.com>
2026-09-12 23:15:18 +0530

    feat: add string and object to arrays using spread operator

---
+++ JAVASCRIPT/index172.js
@@ -0,0 +1,18 @@
+let name = 'sachin kumar';
+
+let char = [];
+char=
+[...char, name];
+console.log(char);
+
+let object = {
+    name: 'sachin kumar',
+    header: "tony",
+    cateogory: 'panasionic',
+    price:234
+}
+let cart = [];
+cart = [...cart, object];
+console.log(cart);
+
+
